a8230c2bd73f16a1b221a069ceabc263b0bb6785
Jonathan Hull <jhull@gbis.com>
2015-07-30 15:40:20 -0700

    Initial Commit

---
+++ ColorDebugViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorDebugViewExample
+//
+//  Created by Jonathan Hull on 7/30/15.
+//  Copyright © 2015 Jonathan Hull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
